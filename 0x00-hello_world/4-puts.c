33026b46f0fd30dfe034aff48da62c6f77c18930
frankline2022 <franklineochieng457@gmail.com>
2023-08-10 06:44:20 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#Include <stdio.h>
+/**
+ * main Aprogramme that prints a line using puts function
+ *Return:0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
+
