7daf305b2ad06e7c25259fd3ac59eb5105509209
frankline2022 <franklineochieng457@gmail.com>
2023-08-10 10:56:03 -0700

    puts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,1 @@
-#Include < stdio.h >
-/**
- * main - A program that prints a line with printf function
- * Return: 0 (succes)
- */
-int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-		return (0);
-}	
+#Include
