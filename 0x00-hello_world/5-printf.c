b7ead70ec9015742a2d11dc730ec8465bbbd20b7
frankline2022 <franklineochieng457@gmail.com>
2023-08-10 11:55:11 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,11 @@
-#Include
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (succes)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}	
