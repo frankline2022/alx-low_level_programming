a99c66685043365be531b78e4b04cce976db21a1
frankline2022 <franklineochieng457@gmail.com>
2023-08-10 12:02:36 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /**
- * main - Entry point
+ * main - A program that prints a line with printf function
  *
  * Return: Always 0 (succes)
  */
@@ -8,4 +8,4 @@ int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}	
+}
